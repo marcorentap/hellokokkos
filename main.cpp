24a86264f468fd351b015c378ae62e865dd3f213
Marc Owen <marcowen20@gmail.com>
2024-02-26 13:33:44 +0900

    main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <Kokkos_Core.hpp>
+
+int main(int argc, char* argv[]) {
+    Kokkos::ScopeGuard guard(argc ,argv);
+
+    Kokkos::View<int[10]> view("A View", 10);
+
+    printf("enter parallel_for");
+    Kokkos::parallel_for(10, KOKKOS_LAMBDA (const int &i) {
+        view(i) = i;
+    });
+
+    printf("print view");
+    Kokkos::parallel_for(10, KOKKOS_LAMBDA (const int &i) {
+        printf("view(i): %d\n", view(i));
+    });
+    return 0;
+}
