0903a57a4e90bfd4400c9c6c9cf9c8eda93c7bbd
Marc Owen <marcowen20@gmail.com>
2024-03-15 21:28:38 +0900

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,2 @@
+sudo KOKKOS_TOOLS_LIBS=/home/marcorentap/projects/mperf-profiler/build/libmkt_region_cpi.so ./build/hellokokkos
+# sudo KOKKOS_TOOLS_LIBS=/usr/local/lib/libmkt_perf_proc.so ./build/hellokokkos
