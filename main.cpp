b87e3a67373be5638ce4907b30a8b6f67558739c
Marc Owen <marcowen20@gmail.com>
2024-04-25 17:54:35 +0900

    Convert size to a constant N

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <Kokkos_Core.hpp>
 #include <impl/Kokkos_Profiling.hpp>
 
+#define N 10
+
 int main(int argc, char* argv[]) {
     Kokkos::ScopeGuard guard(argc ,argv);
     Kokkos::Profiling::pushRegion("Whole Program");
 
-    Kokkos::View<int[10]> view("A View", 10);
+    Kokkos::View<int[N]> view("A View", N);
 
     Kokkos::Profiling::pushRegion("Set Values");
     printf("enter parallel_for\n");
-    Kokkos::parallel_for(10, KOKKOS_LAMBDA (const int &i) {
+    Kokkos::parallel_for(N, KOKKOS_LAMBDA (const int &i) {
         view(i) = i;
     });
     Kokkos::Profiling::popRegion();
 
     Kokkos::Profiling::pushRegion("Print Values");
     printf("print view\n");
-    Kokkos::parallel_for(10, KOKKOS_LAMBDA (const int &i) {
+    Kokkos::parallel_for(N, KOKKOS_LAMBDA (const int &i) {
         printf("view(i): %d\n", view(i));
     });
     Kokkos::Profiling::popRegion();
